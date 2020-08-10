6daee711ac78296c77a4e876d0a0bb0c82a0a73d
Dmitrii <ruslanus866@outlook.com>
2020-08-10 15:32:36 +0800

    Updates

---
+++ precache-manifest.4a67b0f527ab08048b4767e93d2397e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd425bce5b065db30b1ed10b13378fbb",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2604423e631892e49ca3",
+    "url": "/monsters-rolodex/static/css/main.82bda72c.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2604423e631892e49ca3",
+    "url": "/monsters-rolodex/static/js/main.4b51d3af.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
